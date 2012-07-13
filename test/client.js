d48ca2ebbcb3fed31b9d2c9e7fb2018647d4680f
Calvin French-Owen <calvin@segment.io>
2012-07-13 14:46:22 -0700

    Pusing tests

--- test/client.js
+++ test/client.js
@@ -1,48 +1,47 @@
 
-var segmentio = require('segmentio');
+var should = require('should'),
+    Client = require('../lib/client');
 
-describe('Segmentio client', function () {
 
-    segmentio.init();
+var options = {
 
-    //
-    // Identify
-    //
+    host : 'http://localhost:81',
+    flushSize : 1
 
-    it('should identify without traits, and without context', function (done) {
+};
 
+
+describe('Batch', function () {
+
+    var visitorId = 'test@segment.io',
+        sessionId = '123456789',
+        apiKey    = 'fakeid';
+
+    var seg = new Client(apiKey, options);
+
+
+    it('should properly identify', function (done) {
+
+        seg.identify({ visitor : visitorId,
+                       session : sessionId,
+                       traits  : { baller : true }}, function (err, result) {
+
+            should.not.exist(err);
+
+            done();
+        });
     });
 
-    it('should identify with traits, but without context', function (done) {
+    it('should properly track', function (done) {
 
+        seg.track({ visitor : visitorId,
+                    event   : 'Ate a cookie' }, function (err, result) {
+
+            should.not.exist(err);
+
+            done();
+        });
     });
 
-    it('should identify with traits, and with context', function (done) {
-
-    });
-
-    it('should disallow identify without session id or visitor id', function (done) {
-
-    });
-
-    //
-    // Track
-    //
-
-    it('should track without properties', function (done) {
-
-    });
-
-    it('should track without properties', function (done) {
-
-    });
-
-    it('should allow flushing.', function (done) {
-
-    });
-
-    it('should allow flushing.', function (done) {
-
-    });
 
 });

a6a55b68d985835b4a5f0b41b6e54b41910ddc85
Ilya Volodarsky <ivolo@mit.edu>
2012-07-13 00:25:45 -0700

    adding readme and global segmentio default client file

---
+++ test/client.js
@@ -0,0 +1,48 @@
+
+var segmentio = require('segmentio');
+
+describe('Segmentio client', function () {
+
+    segmentio.init();
+
+    //
+    // Identify
+    //
+
+    it('should identify without traits, and without context', function (done) {
+
+    });
+
+    it('should identify with traits, but without context', function (done) {
+
+    });
+
+    it('should identify with traits, and with context', function (done) {
+
+    });
+
+    it('should disallow identify without session id or visitor id', function (done) {
+
+    });
+
+    //
+    // Track
+    //
+
+    it('should track without properties', function (done) {
+
+    });
+
+    it('should track without properties', function (done) {
+
+    });
+
+    it('should allow flushing.', function (done) {
+
+    });
+
+    it('should allow flushing.', function (done) {
+
+    });
+
+});

38f36fc772607beca9675bd4161ffc637af7c690
Prateek Srivastava <f2prateek@gmail.com>
2019-03-25 16:27:19 -0700

    support alias tests

--- .buildscript/e2e.sh
+++ .buildscript/e2e.sh
@@ -8,6 +8,6 @@ else
   echo "Running end to end tests..."
   wget https://github.com/segmentio/library-e2e-tester/releases/download/0.3.1/tester_linux_amd64 -O tester
   chmod +x tester
-  ./tester -path='./cli.js' -skip-fixtures='alias'
+  ./tester -path='./cli.js'
   echo "End to end tests completed!"
 fi

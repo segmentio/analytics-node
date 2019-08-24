5769b5ce6b121c91da5d9862c94a4fce36abd7bd
Daniel Jackins <danieljackins@gmail.com>
2019-08-24 11:08:39 -0700

    make local validation optional

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,14 +1,17 @@
 const tasks = arr => arr.join(' && ')
-
 module.exports = {
   'hooks': {
     'pre-commit': tasks([
       'exec < /dev/tty',
-      `if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
-        echo "CircleCI Configuration Failed Validation." 
-        echo $eMSG 
-        exit 1
-       fi`
+      `if ! [ -x "$(command -v circleci)" ]; then
+        echo "If you want to verify your circleci config precommit, please install
+              the local circleci tool from https://circleci.com/docs/2.0/local-cli/"; else
+        if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
+          echo "CircleCI Configuration Failed Validation." 
+          echo $eMSG 
+          exit 1
+        fi
+      fi`
     ])
   }
 }

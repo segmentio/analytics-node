c3737e5773a2037da2fcc42fd66e0ea5c1ed6658
Daniel Jackins <danieljackins@gmail.com>
2019-08-24 11:11:14 -0700

    fixes message formatting

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,9 +3,8 @@ module.exports = {
   'hooks': {
     'pre-commit': tasks([
       'exec < /dev/tty',
-      `if ! [ -x "$(command -v circleci)" ]; then
-        echo "If you want to verify your circleci config precommit, please install
-              the local circleci tool from https://circleci.com/docs/2.0/local-cli/"; else
+      `if ! [ -x "$(command -v circleci2)" ]; then
+        echo "If you want to verify your circleci config precommit, please install the local circleci tool from https://circleci.com/docs/2.0/local-cli/"; else
         if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
           echo "CircleCI Configuration Failed Validation." 
           echo $eMSG 

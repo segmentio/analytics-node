625b97fcab5aafde6c138cfeb6d4443499d47fa4
Daniel Jackins <danieljackins@gmail.com>
2019-08-26 00:50:32 -0700

    cleans up circleci validation with script

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,13 +3,9 @@ module.exports = {
   'hooks': {
     'pre-commit': tasks([
       'exec < /dev/tty',
-      `if ! [ -x "$(command -v circleci2)" ]; then
+      `if ! [ -x "$(command -v circleci)" ]; then
         echo "If you want to verify your circleci config precommit, please install the local circleci tool from https://circleci.com/docs/2.0/local-cli/"; else
-        if ! eMSG=$(circleci config validate -c .circleci/config.yml); then
-          echo "CircleCI Configuration Failed Validation." 
-          echo $eMSG 
-          exit 1
-        fi
+        yarn run circle-lint
       fi`
     ])
   }

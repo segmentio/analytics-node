7428915df9aefe05feb5acde2ae170ddffe56e0d
Horacio Peña <horape@gmail.com>
2022-11-04 15:21:26 -0300

    allow recovering from failed flushs

--- index.js
+++ index.js
@@ -237,9 +237,13 @@ class Analytics {
    * @return {Analytics}
    */
 
-  async flush (callback) {
-    await this.pendingFlush
-    callback = callback || noop
+  async flush(callback) {
+    {
+      const pending = this.pendingFlush;
+      this.pendingFlush = null;
+      await pending; // this may throw
+    }
+    callback = callback || noop;
 
     if (!this.enable) {
       setImmediate(callback)

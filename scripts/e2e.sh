8db5cbb69d76979983a164fe852b11596851c1b6
Slimane Akalië <30215409+slimaneakalie@users.noreply.github.com>
2023-05-07 15:03:22 +0100

    update github actions

--- scripts/e2e.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Running end to end tests..."
-wget https://github.com/segmentio/library-e2e-tester/releases/download/0.3.2/tester_linux_amd64 -O tester
-chmod +x tester
-./tester -path='./cli.js' -concurrency=3
-echo "End to end tests completed!"
